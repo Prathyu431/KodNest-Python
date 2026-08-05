b923f70ed9da78c6ac4c87be4c917c4dc1255fe2
Prathyu431 <prathyusha64252005@gmail.com>
2026-08-05 17:05:45 +0530

    Range

---
+++ track/MOD2/T02/src/number_word.py
@@ -0,0 +1,27 @@
+number=int(input("Enter the number:"))
+word=input("Enter the word:")
+print("Numbers:")
+for i in range(1,number+1):
+    print(i)
+print("Characters:")
+for j in word:
+    print(j)
+'''
+ Output:
+enter the number:5
+Enter the word:kodnest
+Numbers:
+1
+2
+3
+4
+5
+Characters:
+k
+o
+d
+n
+e
+s
+t
+'''
